7c10c5c0916149b3990cd5d2a4ef8ab451819812
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-18 12:12:00 +0530

    added parameterized constructor to the Pizza type

--- src/main/java/com/niit/jdp/Pizza.java
+++ src/main/java/com/niit/jdp/Pizza.java
@@ -9,4 +9,10 @@ public class Pizza {
     private String name;
     private double price;
     private int sizeInInches;
+
+    public Pizza(String name, double price, int sizeInInches) {
+        this.name = name;
+        this.price = price;
+        this.sizeInInches = sizeInInches;
+    }
 }

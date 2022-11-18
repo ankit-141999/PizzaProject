0ae9f16ca474120532ffa7f1150330269d25d74c
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-18 12:45:51 +0530

    initialized PizzaShop inside main() and called displayPizzaMenu()

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -2,6 +2,7 @@ package com.niit.jdp;
 
 public class Main {
     public static void main(String[] args) {
-
+        PizzaShop pizzaShop = new PizzaShop();
+        pizzaShop.displayPizzaMenu();
     }
 }

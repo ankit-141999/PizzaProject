91eb2de6bab56c2c84163722e5c58947ddd19ddc
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-18 12:25:28 +0530

    created PizzaShop.java, added a constructor, and addPizzaToMenu()

---
+++ src/main/java/com/niit/jdp/PizzaShop.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Pinky Sodhi
+ * Date : 18-11-2022
+ * Created Using : IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp;
+
+import java.util.HashSet;
+
+public class PizzaShop {
+    HashSet<Pizza> pizzaMenu;
+
+    public PizzaShop() {
+        pizzaMenu = new HashSet<>();
+    }
+
+    public boolean addPizzaToMenu(Pizza pizza) {
+        return pizzaMenu.add(pizza);
+    }
+
+    private void initializePizzaMenu() {
+
+    }
+
+}

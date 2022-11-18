54a9e7299b8308e1d548612ff1dc1ef6b45ac64a
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-18 12:11:17 +0530

    added fields to Pizza type

---
+++ src/main/java/com/niit/jdp/Pizza.java
@@ -0,0 +1,12 @@
+/*
+ * Author : Pinky Sodhi
+ * Date : 18-11-2022
+ * Created Using : IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp;
+
+public class Pizza {
+    private String name;
+    private double price;
+    private int sizeInInches;
+}
